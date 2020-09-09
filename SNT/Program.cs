7e078573cf6307d1fa40ab8f0fb83cce2687fc99
VanTrung2000 <70925652+VanTrung2000@users.noreply.github.com>
2020-09-09 19:17:15 +0700

    Update Program.cs

--- SNT/Program.cs
+++ SNT/Program.cs
@@ -1,21 +1,16 @@
-﻿using System;
+using System;
 
 namespace Kiem_tra_so_nguyen_to
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.Write("\n");
-            Console.Write("Kiem tra n co phai la so nguyen to:\n");
-            
-
+        static void kiemtrsonguyento(){
             int s = 0;
-            int n, i;
-
-
+            int n;
+            int i;
             Console.Write("Hay nhap mot so n bat ky: ");
             n = Convert.ToInt32(Console.ReadLine());
+
             for (i = 1; i <= n; i++)
                 if (n % i == 0)
                     s++;
@@ -24,7 +19,10 @@ namespace Kiem_tra_so_nguyen_to
                 Console.WriteLine("So da cho la so nguyen to.");
             else
                 Console.WriteLine("So da cho khong phai la so nguyen to.");
-
+        }
+        static void Main(string[] args)
+        {
+           kiemtrsonguyento();
             Console.ReadKey();
         }
     }
